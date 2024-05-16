27fc6ec7896518af6b7145a3d804cb3956ca83ee
TonyCode <pgktony615@gmail.com>
2024-05-15 19:29:14 -0500

    completed

--- password_generator.py
+++ password_generator.py
@@ -5,19 +5,35 @@ def generate_password(length);
     if length < 6:
         raise ValueError("Password length must be at least 6 characters.")
 
-#define character sets 
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-digits = string.digits
-special = string.punctuation
+    #define character sets 
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    digits = string.digits
+    special = string.punctuation
 
-#Ensure the password includes atleast one character from each set
-all_characters = lower + upper + digits + special
-password = [
-    random.choice(lower),
-    random.choice(upper),
-    random.choice(digits),
-    random.choice(special)
-]
+    #Ensure the password includes atleast one character from each set
+    all_characters = lower + upper + digits + special
+    password = [
+        random.choice(lower),
+        random.choice(upper),
+        random.choice(digits),
+        random.choice(special)
+    ]
 
+    #Fill the remaining length with random choices from all characters
+    password += random.choice(all_characters, k=length-4)
+
+    #shuffle the same result to ensure randomness
+    random.shuffle(password)
+
+    return ''.join(password)
+
+if __name__ == '__main__':
+    try: 
+        length = int(input("Enter the password length: "))
+        print(f"Generated password: {generate_password(length)}")
+    except ValueError as ve:
+        print(ve)
+    except Exception as e:
+        print(f"An error occured: {e}")
 
