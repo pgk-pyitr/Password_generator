8ed1e305aeddc4c7dc864d2e8dfe9c2e587f019a
TonyCode <pgktony615@gmail.com>
2024-05-15 19:21:42 -0500

    Defining characters sets

--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,23 @@
 import random
 import string
 
+def generate_password(length); 
+    if length < 6:
+        raise ValueError("Password length must be at least 6 characters.")
+
+#define character sets 
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+digits = string.digits
+special = string.punctuation
+
+#Ensure the password includes atleast one character from each set
+all_characters = lower + upper + digits + special
+password = [
+    random.choice(lower),
+    random.choice(upper),
+    random.choice(digits),
+    random.choice(special)
+]
+
+
